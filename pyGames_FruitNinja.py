d0491dd7b9d07dff264a6f49162bdc377d25e421
April <136133238+agongster@users.noreply.github.com>
2023-11-05 02:48:42 -0500

    Add files via upload

--- pyGames_FruitNinja.py
+++ pyGames_FruitNinja.py
@@ -7,7 +7,6 @@ level = 0
 mouthWidth = 200
 foods = []
 gravity = 2
-life=10
 
 
 
@@ -21,19 +20,27 @@ poison = pygame.image.load(os.path.join('opencvApp', 'poison.png'))
 meat= pygame.image.load(os.path.join('opencvApp','meat.png'))
 heart=pygame.image.load(os.path.join('opencvApp','heart.png'))
 dragon=pygame.image.load(os.path.join('opencvApp','dragon.png'))
+pat=pygame.image.load(os.path.join('opencvApp', 'pat_virtue.png'))
+mike=pygame.image.load(os.path.join('opencvApp', 'mike_taylor.png'))
 
-pygame.transform.scale(poison,(50,50))
-pygame.transform.scale(meat,(50,50))
-pygame.transform.scale(heart,(20,20))
-pygame.transform.scale(dragon,(500,200))
+poison = pygame.transform.scale(poison,(100,100))
+meat = pygame.transform.scale(meat,(100,100))
+heart = pygame.transform.scale(heart,(50,50))
+dragon = pygame.transform.scale(dragon,(600,200))
+pat = pygame.transform.scale(pat, (175,175))
+mike = pygame.transform.scale(mike, (100,100))
 
-def takeStep():
+def restart():
+    global foods
+    foods = []
+
+def takeStep(life):
     i=0
     while i<len(foods):
         (x, y, vx, vy,image) =foods[i]
         if y>600 and (x<650) and (x>150):
             foods.remove(foods[i])
-            if image==poison and life>=1:
+            if image!=meat and life>=1:
                 return (life-1)
         else:
             x += vx
@@ -45,6 +52,7 @@ def takeStep():
 def drawBalls():
     for (x, y, vx, vy,image) in foods:
         WIN.blit(image,(x,y))
+    
 def launchFood(): #add food data to foods
     if random.random() > 0.5: #launch from left
         x0 = -100 + random.random()*200
@@ -55,36 +63,38 @@ def launchFood(): #add food data to foods
     t = (2*gravity + math.sqrt(4*gravity**2 + 32*h*gravity))/(2*gravity)
     vx = (x1-x0)/t
     vy = -0.5*gravity*t
-    imageList=[poison,poison,poison,meat,poison,meat,poison]
+    imageList=[mike,pat,mike,meat,pat,meat,mike, pat]
     image=imageList[random.randrange(len(imageList))]
     foods.append((x0, 800, vx, vy,image))
 
-def redrawAll():
-    for i in range(life):
-                WIN.blit(heart,(25*(i+1),25*(i+1)))
-    WIN.blit(dragon,(400,700))
+def redrawAll(life):
+    print(life)
     WIN.fill((255, 255, 255))
     drawBalls()
-    # for fjfjfj:
-    #     draw(spaceship, (x1, y1))
+    for i in range(life):
+        WIN.blit(heart,(25*(i+1),25))
+    WIN.blit(dragon,(100,600))
     pygame.display.update()
 
 def main():
     gameOver=False
     counter=0
     speed=50
+    life = 10
     clock = pygame.time.Clock()
     run = True
     while run:
         clock.tick(FPS)
-        redrawAll()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
         
         keysPressed = pygame.key.get_pressed()
         if keysPressed[pygame.K_r]:
-            gameOver = not gameOver
+            life=10
+            restart()
+            gameOver = False
+            # redrawAll(life)
 
         if gameOver==False:
             
@@ -92,10 +102,14 @@ def main():
                 launchFood()
             if counter/750==0 and speed>5:
                 speed/=2
-            takeStep()
+            result = takeStep(life)
+            if isinstance(result,int):
+                life=result
             if life<=0:
                 gameOver=True
             counter+=1
+        redrawAll(life)
+
     pygame.quit()
 
 if __name__ == "__main__":
